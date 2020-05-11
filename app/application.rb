d9869fbb355afc4f29af4ee443a1924bde57ab99
klarissasmith <ksmith_email@yahoo.com>
2020-05-10 17:39:03 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+require 'pry'
+class Application
+    def call(env)
+        resp = Rack::Response.new
+        if Time.now.hour < 12
+            # binding.pry
+            resp.write "Good Morning"
+        else
+            resp.write "Good Afternoon"
+        end
+        resp.finish
+    end
+end
